7f6b5baa6c92e25453baa596de34e02259c2daf8
Jaycoder101256 <118722957+Jaycoder101256@users.noreply.github.com>
2023-10-24 17:25:24 +0530

    j

--- logEvents.js
+++ logEvents.js
@@ -1,23 +1,21 @@
 const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
-
-const fs = require('fs');
-const fsPromises = require('fs').promises;
+const fs = require('fs'); // Import 'fs' for synchronous file system operations
+const fsPromises = require('fs').promises; // Import 'fs.promises' for asynchronous file system operations
 const path = require('path');
 
-const logEvents = async (message, logName) => {
-    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
-    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
-
+const logEvents = async(message) => {
+    const datetime = ` ${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItems = ` ${datetime}\t${uuid()}\t${message}`;
+    console.log(logItems);
     try {
         if (!fs.existsSync(path.join(__dirname, 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, 'logs'));
         }
-
-        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventlog.txt'), logItems);
     } catch (err) {
-        console.log(err);
+        console.error(err); // Use console.error for error logging
     }
-}
+};
 
 module.exports = logEvents;
